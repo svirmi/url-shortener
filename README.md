1629870d458c9e57f8b6e083e43d3156d4475053
M S <mixa.ru.ru@gmail.com>
2021-04-23 08:44:06 +0300

    backend

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# url-shortener
-url shortener code (just as reminder to keep code somewhere), not production-ready
+# url-shortener (Go Fiber)
+url shortener code (just as reminder to keep code pieces somewhere), not production-ready
--- main.go
+++ main.go
@@ -4,16 +4,52 @@ import (
 	"log"
 
 	"github.com/gofiber/fiber/v2"
-	_ "github.com/google/uuid"
-	_ "github.com/prologic/bitcask"
+	"github.com/google/uuid"
+	"github.com/prologic/bitcask"
 )
 
+var db *bitcask.Bitcask
+
+type shorten struct {
+	URL string
+}
+
+func shortenHandler(ctx *fiber.Ctx) error {
+	var id string
+
+	set := new(shorten)
+	err := ctx.BodyParser(&set)
+
+	if err != nil {
+		return err
+	}
+
+	for {
+		id = uuid.NewString()[:5]
+		if _, err := db.Get([]byte(id)); err != nil {
+			// exit the loop if no such an ID in db
+			break
+		}
+	}
+
+	err = db.Put([]byte(id), []byte(set.URL))
+
+	if err != nil {
+		return err
+	}
+
+	return ctx.SendString(id)
+}
+
 func main() {
+	db, _ = bitcask.Open("db")
 	app := fiber.New()
 
 	app.Get("/", func(ctx *fiber.Ctx) error {
 		return ctx.SendFile("public/index.html")
 	})
+
+	app.Post("/api/v1", shortenHandler)
 
 	log.Fatal(app.Listen(":3000"))
 }
